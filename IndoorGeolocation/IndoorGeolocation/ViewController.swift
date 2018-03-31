8c81b498db58c7533175b132bb23f815dd902d4a
Ruben Garcia <ruben.garcia@CO-IT01670.local>
2018-03-31 10:29:04 -0500

    beacons in testing

--- IndoorGeolocation/IndoorGeolocation/ViewController.swift
+++ IndoorGeolocation/IndoorGeolocation/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
+
+
+// Create LocationManager object
+// Set a reason as to why we're asking for that in the info.plist
+// Request authorization on the device
+// ClRegion - All the beacon's Data
+// If the user authorized, then we're going to start ranging for the beacon we've set up
+// Read the proximity property on the beacon as it's ranged and change the screen colour based on that
 
 class ViewController: UIViewController {
+    var locaticonManager: CLLocationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Request authorization on the device
+        locaticonManager.delegate = self
+        locaticonManager.requestAlwaysAuthorization()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func rangeBeacons() {
+        let uuid = UUID(uuidString: "Beacon uuid")
+        let major: CLBeaconMajorValue = 31745
+        let minor: CLBeaconMinorValue = 64019
+        let identifier = "fm.combo.livingRoomBeacon"
+        
+        let region = CLBeaconRegion(proximityUUID: uuid!, major: major, minor: minor, identifier: identifier)
+        locaticonManager.startRangingBeacons(in: region)
     }
-
-
+    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            // User has authorized the application - range those beacons!
+            rangeBeacons()
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        guard let discoveredBeaconProximity = beacons.first?.proximity else {print("Couldn't find the beacon!"); return}
+        let backgroundColour: UIColor = {
+            switch discoveredBeaconProximity {
+                case .immediate: return UIColor.green
+                case .near: return UIColor.orange
+                case .far: return UIColor.red
+                case .unknown: return UIColor.black
+            }
+        }()
+        view.backgroundColor = backgroundColour
+    }
+}
---
+++ IndoorGeolocation/Pods/Target Support Files/Pods-IndoorGeolocation/Pods-IndoorGeolocation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IndoorGeolocation : NSObject
+@end
+@implementation PodsDummy_Pods_IndoorGeolocation
+@end
